492ac427ed332e0c7ef146b005aff3dc3e540e38
0xsuid <0xsuid@protonmail.com>
2023-10-06 16:15:02 +0200

    Rename db.php to EnvLoader.php

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ if (empty($_SESSION)) {
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Load Config
-require_once __DIR__ . '/../src/config/DB.php';
+require_once __DIR__ . '/../src/config/EnvLoader.php';
 
 // Add Routes
 require_once __DIR__ . '/../src/routes/Web.php';
--- src/config/EnvLoader.php
+++ src/config/EnvLoader.php
@@ -5,10 +5,13 @@ $dotenv = Dotenv\Dotenv::createMutable(__DIR__ . '/../../');
 $dotenv->load();
 
 // Retrive env variable
+$appDebug = filter_var($_ENV['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ?: false;
 $dbHost = $_ENV['DB_HOST'] ?: 'localhost';
 $dbUser = $_ENV['DB_USER'] ?: 'root';
 $dbPass = $_ENV['DB_PASS'] ?: '';
 $dbName = $_ENV['DB_NAME'] ?: 'blog';
+
+define('APP_DEBUG', $appDebug);
 
 // Database params
 define('DB_HOST', $dbHost);
