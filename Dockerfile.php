7b4a06d5de25054e5b89d44dce2d0b1277ee8055
0xsuid <0xsuid@protonmail.com>
2023-10-06 16:44:14 +0200

    Update Dockerfile - Add composer installation

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,12 +1,28 @@
 FROM php:8.2-apache
 
-RUN docker-php-ext-install pdo pdo_mysql && docker-php-ext-enable pdo_mysql
-
 ENV APACHE_DOCUMENT_ROOT=/var/www/html/public
 
 RUN sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf
 RUN sed -ri -e 's!/var/www/!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
 
+RUN a2enmod rewrite
+
+RUN apt-get update \
+    && apt install -y git libzip-dev zip \
+    && docker-php-ext-install pdo pdo_mysql zip \
+    && docker-php-ext-enable pdo_mysql
+
 COPY . /var/www/html
+
+COPY --from=composer:latest /usr/bin/composer /usr/local/bin/composer
+
+RUN composer install \
+    --no-interaction \
+    --no-plugins \
+    --no-scripts \
+    --no-dev \
+    --prefer-dist
+
+RUN composer dump-autoload
 
 RUN chown -R www-data:www-data /var/www/html
